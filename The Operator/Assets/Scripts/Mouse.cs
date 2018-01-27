ed56d509db1b39540d55dc01d5f7f51daca75486
Christian Oliveros <christianol_01@hotmail.com>
2018-01-27 11:59:09 +0100

    First git push

---
+++ The Operator/Assets/Scripts/Mouse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Mouse : MonoBehaviour {
+
+    LineRenderer line;
+    public float line_distance = 100f;
+    public LayerMask ignore_mask;
+    public Transform main_board;
+    // Use this for initialization
+    void Start () {
+        line = GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, ignore_mask.value))
+        {
+            line.enabled = true;
+            line.SetPosition(0, hit.point - main_board.forward * line_distance);
+            line.SetPosition(1, hit.point);
+        }
+        else
+        {
+            line.enabled = false;
+        }
+    }
+}
---
+++ The Operator/Assets/Scripts/RopeScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour {
+
+    public Rigidbody RBody;
+	// Use this for initialization
+	void Start () {
+        this.gameObject.AddComponent<Rigidbody>();
+        this.RBody = this.gameObject.GetComponent<Rigidbody>();
+        this.RBody.isKinematic = true;
+
+        int childCount = this.transform.childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            
+            Transform transform = this.transform.GetChild(i);
+            if (transform.gameObject.name != "Anchor")
+            {
+                transform.gameObject.AddComponent<HingeJoint>();
+                transform.gameObject.AddComponent<Rigidbody>();
+
+                HingeJoint hingeJoint = transform.gameObject.GetComponent<HingeJoint>();
+
+                hingeJoint.connectedBody = i == 0 ? this.RBody : this.transform.GetChild(i - 1).GetComponent<Rigidbody>();
+
+                hingeJoint.useSpring = true;
+
+                hingeJoint.enableCollision = false;
+            }
+            //Last joint
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
