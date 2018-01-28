3294e369f6c0bd14fe4abb3ec27ed9388f660b83
Fehime <fese@itu.dk>
2018-01-28 04:45:11 +0100

    DictatorWin text added
    Dictator win text added

--- Assets/Scripts/EndingHandler.cs
+++ Assets/Scripts/EndingHandler.cs
@@ -31,7 +31,7 @@ public class EndingHandler : MonoBehaviour
 		ending_background.gameObject.SetActive(false);
 		resetButton.SetActive(false);
 
-		goverment_ending = "Government Won";
+		goverment_ending = "Mr. President proved his power one more time! Our legendary army forced back all the rebel movement. These traitors couldn’t managed to kill our democracy. Live long the president! ";
 		fired_ending = "The best citizen is the one who does her job best. The news of the day is about the ones who doesn’t do any work. It is understandable that everybody can have personal issues sometimes that affecting the work quality. Anyone may be having a difficult time. But while the hole country is having hard time you cannot be stuck on your personal problems. The humanity is waiting for your service. So the ones who fired, before blaming on your bosses, look back to your performance and be honest to yourself. ";
 		nothing_ending = "Nobody is expecting from you to change the destiny. Of course you cannot save everybody but C’MON! You haven’t done anything at all!! No effect on the history.";
 		rebel_ending = "The history is being rewritten today. After the years of dictatorship, our country is having a new beginning. The uprising of crowd against the former government  became successful. We had an interview with the lead of rebels. \n\tRebel Leader: “This wouldn’t be the result without the unknown hero working in front   of the switch board. If she transmitted our calls to government we’d be having our darkest days at the moment. Feel the love transmitting from us to you the hero!” \n";
