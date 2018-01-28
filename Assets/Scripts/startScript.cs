2a03dbb5b306dd70684dff5c879eb59160abb57a
Orom <victor.olsson123@gmail.com>
2018-01-28 03:23:02 +0100

    Scene changes

---
+++ Assets/Scripts/startScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScript : MonoBehaviour {
+
+	public void startGame()
+    {
+        Debug.Log("START GAME");
+       // SceneManager.
+        Application.LoadLevel(1);
+    }
+}
